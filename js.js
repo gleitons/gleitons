ef94a340945c23c439c86f77ef3eb37bfa1fbcac
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-15 17:40:14 -0300

    atia

---
+++ js.js
@@ -0,0 +1,12 @@
+function colapse() {
+    const acc = document.getElementById('questionsAccordeon');
+
+    acc2 = acc('click',)
+
+    if (acc==1) {
+        acc.classList.remove('oculto')
+      } else {
+        acc.classList.add('oculto')
+      }
+}
+
